78d6fadee54a3f835396613f3a94d86da800dd03
shradhasr <68332042+shradhasr@users.noreply.github.com>
2020-07-15 22:12:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,12 +4,15 @@ conn = sqlite3.connect('TEST.db')
 #conn = sqlite3.connect(':memory:')
 
 c= conn.cursor()
-c.execute(""" CREATE TABLE EMP(
-		FIRST_NAME TEXT,
-		LAST_NAME TEXT,
-		PAY INTEGER
-	)""")
-
+#c.execute(""" CREATE TABLE EMP(
+#		FIRST_NAME TEXT,
+#		LAST_NAME TEXT,
+#		PAY INTEGER
+#	)""")
+c.execute("Insert into EMP values ('abc', 'last', 123)"
+c.execute("Insert into EMP values ('abcd', 'last2', 1234)")
+c.execute("Select * from EMP")
+print(c.fetchall())
 
 conn.commit()
 conn.close()
