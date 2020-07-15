3fab7d4a576082f48e2f5d13cb0e504e0a6ba6a7
shradhasr <68332042+shradhasr@users.noreply.github.com>
2020-07-15 21:47:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello I am from python")
+import sqlite3
+
+conn = sqlite3.connect('employee.db')
+
+c=conn.cursor()
+
+c.execute(""" CREATE TABLE EMP(
+		FIRST_NAME TEXT,
+		LAST_NAME TEXT,
+		PAY INTEGER
+	)""")
+CONN.COMMIT()
+
+CONN.CLOSE()
