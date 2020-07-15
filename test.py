9910a22000c0fb8e5a4bbb24855cbae6996e5a76
shradhasr <68332042+shradhasr@users.noreply.github.com>
2020-07-15 21:59:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 import sqlite3
 
-conn = sqlite3.connect('employee.db')
+#conn = sqlite3.connect('employee.db')
+conn = sqlite3.connect(':memory:')
 
-c=conn.cursor()
-
+c= conn.cursor()
 c.execute(""" CREATE TABLE EMP(
 		FIRST_NAME TEXT,
 		LAST_NAME TEXT,
 		PAY INTEGER
 	)""")
-CONN.COMMIT()
 
-CONN.CLOSE()
+
+conn.commit()
+conn.close()
