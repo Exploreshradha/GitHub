6cdbba8932d5d6917129ecd7d47944c5216736a3
shradhasr <68332042+shradhasr@users.noreply.github.com>
2020-07-16 11:57:16 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import sqlite3
+import csv
+import pandas
 
 conn = sqlite3.connect('TEST.db')
 #conn = sqlite3.connect(':memory:')
 
 c= conn.cursor()
-#c.execute(""" CREATE TABLE EMP(
-#		FIRST_NAME TEXT,
-#		LAST_NAME TEXT,
-#		PAY INTEGER
-#	)""")
+c.execute(""" CREATE TABLE EMP(
+		FIRST_NAME TEXT,     LAST_NAME TEXT, 		PAY INTEGER 	)""")
 c.execute("Insert into EMP values ('abc', 'last', 123)")
 c.execute("Insert into EMP values ('abcd', 'last2', 1234)")
 c.execute("Select * from EMP")
-print(c.fetchall())
+#print(c.fetchall())
+#d='Select * from EMP'
+result= pandas.read_sql_query("Select * from EMP",conn)
+result.to_csv("EmpTable.CSV")
 
 conn.commit()
 conn.close()
